9153d98453d5fadca3ef6a14b8111360443d6634
Retrasad <prostachok83@bk.ru>
2025-04-30 17:26:34 +0300

    main part

---
+++ Orlov_project_LR4.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+double da()
+{
+    double ntr;
+    while (true) {
+        cin >> ntr;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Похоже, ввод был ошибочным. Попробуйте ввести число ещё раз \n";
+        }
+        else {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            break;
+        }
+    }
+    return ntr;
+}
+ 
+ 
+int da1()
+{
+    int ntr;
+    while (true) {
+        cin >> ntr;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Похоже, ввод был ошибочным. Попробуйте ввести число ещё раз \n";
+        }
+        else 
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            break;
+        }
+    }
+    return ntr;
+}
+ 
+ 
+double veshx()
+{
+ 
+}
+ 
+ 
+int naty()
+{
+ 
+}
+ 
+ 
+double okrugx(double x, int y)
+{
+ 
+}
+ 
+ 
+double drobchast(double x)
+{
+ 
+}
+ 
+ 
+ 
+ 
+ 
+void menu()
+{
+    char cont = 'y';
+    while (cont == 'y' or cont == 'Y')
+    {
+        int nt;
+        while (true)
+        {
+            cout << "Введите номер желаемого процесса вычисления (1-4):" << endl;
+            cout << "1. Ввод вещественного числа x. \n"; 
+            cout << "2. Ввод натурального числа y. \n"; 
+            cout << "3. Округление x до y разрядов. \n"; 
+            cout << "4. Отбрасование целой части числа x. \n"; 
+            nt = da1();
+            if (nt)
+            {
+                if (nt >= 1 and nt <=4)
+                {
+                    break;
+                }
+                else
+                {
+                    cout << "ТАКОГО НЕТ! Выберите задание от 1 до 4." << endl;
+                }
+            }
+            else
+            {
+                cout << "Ввод должен быть числом." << endl;
+            }
+        };
+        switch (nt)
+        {
+            case 1:
+                veshx();
+                break;
+            case 2:
+                naty();
+                break;
+            case 3:
+                okrugx();
+                break;
+            case 4:
+                drobchast();
+                break;
+            default:
+                cout << "ТАКОГО НЕТ! Выберите задание от 1 до 4." << endl;
+        }
+        cout << "Хотите запустить программу вновь? (y/n):" << endl;
+        cin >> cont;
+    }
+    cout << "Программа завершена." << endl;
+ }
+ 
+ 
+ 
+int main()
+{
+    menu();
+    return 0;
+}
